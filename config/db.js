5718590de12fa427530bb08797b30b0b67b29476
Nigar0826 <ahmedovanf@gmail.com>
2024-10-11 14:13:20 -0400

    Updated MongoDB connection to use a correct  environment variable

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // Connect to Mongodb using the MongoDB Atlas connection string
-        await mongoose.connect('mongodb+srv://nigarahmadova:qCOep5jmKJfanU7E@comp3123assignment1.eyrcw.mongodb.net/comp3123assignment1?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
